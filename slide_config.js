7ab94daad7e66647079422f1a01618a662421bc6
Jeff Rose <jeff.rose12@gmail.com>
2017-06-03 11:28:49 -0700

    init

---
+++ slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'things',
+    subtitle: 'things and stuff',
+    eventInfo: {
+      title: 'Lightning Talk',
+      date: '1/29/2015'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/rickmug.jpg',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jeff Rose',
+    company: 'Person<br>Place',
+    twitter: '@xjeffrose',
+    www: 'http://xjdr.github.io',
+    github: 'http://github.com/xjdr'
+  }]
+};
